c7e9ef1df6c8208cb764260aebf87192dc78218d
Nicholas Mutale <nicomutale@gmail.com>
2022-04-13 13:04:01 +0200

    update categories

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,17 +1,11 @@
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { categoriesStatus } from '../redux/categories/categories';
 
-const Categories = () => {
-  const Categories = useSelector((state) => state.categoriesReducer);
-  const dispatch = useDispatch();
+const Categories = () => (
+  <>
 
-  return (
-    <>
-      <button type="button" onClick={() => dispatch(categoriesStatus())}>Check status</button>
-      {Categories}
-    </>
+    <p>Under construction</p>
 
-  );
-};
+  </>
+
+);
 export default Categories;
