abddd31a3e8fa61f02ccacda587cf05411c91438
Nicholas Mutale <nicomutale@gmail.com>
2022-04-13 15:44:46 +0200

    update configureStore

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -5,10 +5,6 @@ import logger from 'redux-logger';
 
 import bookReducer from './books/books';
 
-const reducer = combineReducers({
-  bookReducer,
-});
 
-const store = createStore(reducer, applyMiddleware(logger));
 
 export default store;
