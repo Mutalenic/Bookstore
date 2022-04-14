fc8d93a103eb9eaba52b55ed2f13eb1198f0ff16
Nicholas Mutale <nicomutale@gmail.com>
2022-04-14 14:26:03 +0200

    use redux in react update readme

--- src/App.js
+++ src/App.js
@@ -5,24 +5,12 @@ import Navbar from './components/Navbar.js';
 import Books from './components/Books.js';
 import Categories from './components/Categories.js';
 
-const books = [
-  {
-    id: 1,
-    genre: 'genre',
-    title: 'book title',
-    author: 'author',
-    status: 'complete',
-    currentChapter: 'Current chapter',
-    chapterNumber: 'Chapter #number',
-  },
-];
-
 export default function App() {
   return (
     <div className="container">
       <Navbar />
       <Routes>
-        <Route exact path="/" element={<Books books={books} />} />
+        <Route exact path="/" element={<Books />} />
         <Route exact path="/categories" element={<Categories />} />
       </Routes>
     </div>
--- src/redux/categories/categories.js
+++ src/redux/categories/categories.js
@@ -1,15 +1,12 @@
-// Actions
 const CHECKSTATUS = 'bookstore/categories/checkStatus';
 
 const initialState = [];
 
-// Action Creators
 export const checkStatus = (payload) => ({
   type: CHECKSTATUS,
   payload,
 });
 
-// Reducer
 const categoriesReducer = (state = initialState, action) => {
   switch (action.type) {
     case CHECKSTATUS:
